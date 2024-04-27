38a7e0dc358dfb1b99dc6230fba69388e8380898
Sebastian <sebocruz@hotmail.com>
2024-04-27 09:48:10 -0500

    Tested transition of the pop-up card

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const API = ' www.themealdb.com/api/json/v1/1';
+let popUpBox = document.getElementById('popUp');
+const button = document.querySelector('.main__submit-button');
+
+//Display POP-UP
+function displayPopUp() {
+    console.log('clicked');
+  popUpBox.classList.add("display");
+}
+
+//Close POP-UP
+function closePopUp() {
+  popUpBox.classList.remove("display");
+}
+
+button.addEventListener("click",displayPopUp);
